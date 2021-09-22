59917bb40544203e58f3fcbd6ebee9f5cd2b3b55
Rae Dickerson <33698820+TheAmazinRaven@users.noreply.github.com>
2021-09-22 10:33:49 -0500

    9.22.21 Added dir for new course "Java Programming" by Angie Jones.

--- Geeks for Geeks/TrigCalc/src/sqrt2.java
+++ Geeks for Geeks/TrigCalc/src/sqrt2.java
@@ -1,4 +1,18 @@
 public class sqrt2 {
+    public static void main(String[] args) {
+
+        double positiveInfinity= Double.POSITIVE_INFINITY;
+        double negativeVal = -5;
+        double nan = Double.NaN;
+        double result;
+
+        // here argument is negative
+        // output will be NaN
+
+        result = Math.sqrt(negativeVal);
+
+
+    }
 }
 
 // to show working of java.lang.Math.sqrt() method
