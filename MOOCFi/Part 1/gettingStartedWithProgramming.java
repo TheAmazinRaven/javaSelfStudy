d6021846a6366107482ddb80097a1317802c18eb
TheAmazinRaven <raejdickerson@gmail.com>
2022-04-26 01:18:03 +0000

    Testing config.

--- MOOCFi/Part 1/gettingStartedWithProgramming.java
+++ MOOCFi/Part 1/gettingStartedWithProgramming.java
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-public class Sandbox {
-    public static void main(String[] args) {
-        // Write some code
-=======
 public gettingStartedWithProgramming {
     public static void main (String[] args) {
-        // Write some code here.
->>>>>>> fa89d474270bd7378ccb68f0dfdfb33c561dfa39
+        System.out.println("Hello World!");
+
     }
 }
--- README.md
+++
@@ -1,4 +0,0 @@
-# javaSelfStudy
-My self study endeavors so that I can get better at Java outside of coding for class.
-
-9.19.21 - GFG Trig Functions tutorial, Master Java Prog setup
